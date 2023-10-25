f92768877b9c08d9ea040db7c98bdd3881addef8
Chaipat J. <work.chaipat@gmail.com>
2023-10-26 02:58:15 +0700

    + update runscript

--- scripts/run.bat
+++ scripts/run.bat
@@ -1,16 +1,28 @@
 @echo off
-echo ---
+cls
 
-rem replace file with your .asm file
+rem Replace "your_program.asm" with the name of your .asm file
+set source_file=your_program.asm
 
 rem Assemble the source code using MASM
-masm.exe file;
+masm.exe %source_file%;
 
 rem Link the assembled code to create the executable
-link file;
+link %source_file%.OBJ;
+
+echo --- OUTPUT ---
+echo.
 
 rem Run the executable
-file
+%source_file%
 
-echo --- 
+echo.
+echo ---- EOF ----
 
+rem Delete the intermediate files created during assembly and linking
+del %source_file%.OBJ
+del %source_file%.EXE
+
+rem Pause to see the output and any errors
+pause
+cls
